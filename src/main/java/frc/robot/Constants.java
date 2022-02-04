59101b1bcd963ffa2f8226fbe078806d8e006bc1
Riley Garr <60162278+garril23@users.noreply.github.com>
2022-02-03 19:59:44 -0600

    added managemnt subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,11 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class magamentconstants{
+        public static final int magamentoterID = 0;
+        public static final double managerrunspeed = 1;
+        public static final double magegerfullstop = 0;
+
+    }
+}
--- src/main/java/frc/robot/subsystems/ManagementSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManagementSubsystem.java
@@ -1,14 +1,27 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.magamentconstants;
 
-public class ManagementSubsystem extends SubsystemBase{
+public class ManagementSubsystem extends SubsystemBase {
 
-    public ManagementSubsystem() {}
+  private WPI_VictorSPX managenermoter = new WPI_VictorSPX(magamentconstants.magamentoterID);
 
-    @Override
-    public void periodic() {
-      // This method will be called once per scheduler run
+  public ManagementSubsystem() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void toggelmagement(boolean toggel){
+    if (toggel) {
+      managenermoter.set(magamentconstants.managerrunspeed);
+    }else{
+      managenermoter.set(magamentconstants.magegerfullstop);
     }
-    
+
+  }
 }
