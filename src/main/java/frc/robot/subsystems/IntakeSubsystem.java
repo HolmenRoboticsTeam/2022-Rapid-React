5f4b7910827fbf023b14f7f7d6e39186375c096b
Max Stein <98436416+silverstone22@users.noreply.github.com>
2022-02-03 19:46:37 -0600

    intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,8 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase{
+
+  private WPI_VictorSPX intakeMotor = new WPI_VictorSPX(3);
 
     public IntakeSubsystem() {}
 
@@ -10,5 +14,13 @@ public class IntakeSubsystem extends SubsystemBase{
     public void periodic() {
       // This method will be called once per scheduler run
     }
+
+    public void setPosition(boolean open) {
+      if (open) {
+          intakeMotor.set(-1);
+      } else {
+          intakeMotor.set(1);
+      }
+  }
     
 }
